2fdd945b667c3ae98031305f01809994d1ac81f4
Joshua-C-Bryant <josh.c.bryant1@gmail.com>
2021-10-05 13:57:49 -0500

    updated prepare

--- prepare.py
+++ prepare.py
@@ -10,7 +10,7 @@ def prep_store_data(df):
     # convert sales date to datetime
     # df.sale_date = pd.to_datetime(df.sale_date).dt.date
     # df['sale_date']= pd.to_datetime(df['sale_date'])
-    df_store.sale_date = pd.to_datetime(df_store.sale_date.apply(lambda x: x[:-13]))
+    df.sale_date = pd.to_datetime(df.sale_date.apply(lambda x: x[:-13]))
     # create month column
     df['month'] = df.sale_date.dt.month
     # create day of the week column
